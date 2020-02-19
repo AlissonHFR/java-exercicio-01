32f7311cc8ab71794d3bb2557fd870073899083e
Matuk <mateusmatuk@fai-mg.br>
2020-02-19 20:38:17 -0300

    Segundo commit

--- Valores/src/valores/Main.java
+++ Valores/src/valores/Main.java
@@ -6,19 +6,51 @@ public class Main {
 
 	
 	
-	public static void leitura(){
-		Scanner ler = new Scanner(System.in);
-		int n1,n2,n3,n4;
+	private static Scanner ler;
+
+	public static void leitura(String args[]){
 		
-		System.out.print("Digite um valor: ");
+		ler = new Scanner(System.in);
 		
-		n1 = ler.nextInt();
-		n2 = ler.nextInt();
-		n3 = ler.nextInt();
-		n4 = ler.nextInt();
+			int n1,n2,n3,n4;
+			
+			int i = 1;
+			
+			while( i < 5) {
+				System.out.println("Digite um valor: ");
+				switch(i) {
+				case 1: n1 = ler.nextInt();
+					break;
+				case 2: n2 = ler.nextInt();
+					break;
+				case 3: n3 = ler.nextInt();
+					break;
+				case 4:	n4 = ler.nextInt();
+					break;
+				}
+				
+				i++;
+			}
+			 
+			 
+			 
+		
+		
 	}
 	
 	public static void calculo() {
+		
+		int maior, menor;
+		
+		if(maior > 50) {
+			System.out.println("O menor valor é: " + menor);
+			System.out.println("O maior valor é: " + maior);
+			System.out.println("Valor muito alto");
+		}else {
+			System.out.println("O menor valor é: " + menor);
+			System.out.println("O maior valor é: " + maior);
+			System.out.println("Valor normal");
+		}
 		
 	}
 
