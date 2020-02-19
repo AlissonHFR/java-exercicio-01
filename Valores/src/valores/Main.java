00e3a74c598ba96f52e542d4780b7eed81890e3e
Matuk <mateusmatuk@fai-mg.br>
2020-02-19 20:11:18 -0300

    Primeiro commit

---
+++ Valores/src/valores/Main.java
@@ -0,0 +1,25 @@
+package valores;
+
+import java.util.Scanner;
+
+public class Main {
+
+	
+	
+	public static void leitura(){
+		Scanner ler = new Scanner(System.in);
+		int n1,n2,n3,n4;
+		
+		System.out.print("Digite um valor: ");
+		
+		n1 = ler.nextInt();
+		n2 = ler.nextInt();
+		n3 = ler.nextInt();
+		n4 = ler.nextInt();
+	}
+	
+	public static void calculo() {
+		
+	}
+
+}
